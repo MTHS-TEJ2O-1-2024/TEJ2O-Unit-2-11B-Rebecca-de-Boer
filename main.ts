18224f0d942006cf9e48f87258f1d90def47ba15
Rebecca-de-Boer <rebecca.de-boer@mths.ca>
2024-11-05 11:27:13 -0500

    shows and compares

--- main.ts
+++ main.ts
@@ -5,4 +5,29 @@
  * This program ...
 */
 
-basic.showString('Hello, World!')
+let randomNumber: number = 0
+let randomNumber1: number = 0
+let randomNumber2: number = 0
+
+// setup
+basic.clearScreen()
+
+// generating random numbers
+input.onButtonPressed(Button.A, function () {
+    randomNumber1 = randint(0, 99)
+    basic.showNumber(randomNumber)
+})
+
+input.onButtonPressed(Button.B, function () {
+    randomNumber2 = randint(0, 99)
+    basic.showNumber(randomNumber)
+})
+
+// comparing
+input.onGesture(Gesture.Shake, function () {
+    if (randomNumber1 > randomNumber2) {
+        basic.showString(randomNumber1.toString() + " > " + randomNumber2.toString())
+    } else {
+        basic.showString(randomNumber1.toString() + " < " + randomNumber2.toString())
+    }
+})
